16f7aab3357dd85d7c7bb8062370067b2ef06cf2
Faizaa01 <faizaniha0062@gmail.com>
2025-11-05 22:47:05 +0600

    Temporarily disabled email

--- events/signals.py
+++ events/signals.py
@@ -9,13 +9,13 @@ User = get_user_model()
 @receiver(m2m_changed, sender=Event.participants.through)
 def rsvp_activation_mail(sender, instance, action, pk_set=None, **kwargs):
     if action == 'post_add':
-        participant_emails = [user.email for user in User.objects.filter(pk__in=pk_set)]
-        # print("RSVP email check....", participant_emails)
-        send_mail(
-            "RSVP Confirmation",
-            f"Congratulations!!! You have successfully Reserved to the event: {instance.name}",
-            "faizaniha0062@gmail.com",
-            participant_emails,
-            fail_silently=False,
-        )
+        return  # Temporarily disabled email sending
+        # participant_emails = [user.email for user in User.objects.filter(pk__in=pk_set)]
+        # send_mail(
+        #     "RSVP Confirmation",
+        #     f"Congratulations!!! You have successfully Reserved to the event: {instance.name}",
+        #     "faizaniha0062@gmail.com",
+        #     participant_emails,
+        #     fail_silently=False,
+        # )
 
